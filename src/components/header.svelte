9d017d2dfbf70120c3a62bd5a37e6278213c8e17
n17r053v3n <n17r053v3n@protonmail.com>
2022-12-14 20:22:44 +0100

    Mobile header and menu

---
+++ src/components/header.svelte
@@ -0,0 +1,71 @@
+<script>
+    import ModalMenu from "./modalMenu.svelte";
+    let active = false;
+</script>
+
+<div class="wrap nav-wrap">
+    <div class="nav-img">
+        <img src="/img/sys/logo-dark.png" alt="Logo" />
+    </div>
+    <div class="nav-menu nav-menu-desktop">
+        <a href="#proc-my" class="nav-menu-item">Proč my</a>
+        <a href="#o-systemu" class="nav-menu-item">O systému</a>
+        <a href="#cenik" class="nav-menu-item">Ceník</a>
+        <a href="#kontakt" class="nav-menu-item">Kontakt</a>
+    </div>
+    <div class="nav-menu nav-menu-mobile">
+        <button
+            class="hamburger hamburger--boring {active ? 'is-active' : ''}"
+            type="button"
+            on:click={() => (active = !active)}
+        >
+            <span class="hamburger-box">
+                <span class="hamburger-inner" />
+            </span>
+        </button>
+    </div>
+</div>
+{#if active}
+    <ModalMenu/>
+{/if}
+
+<style lang="scss">
+    @import "../styles/hamburgers.css";
+    @import "../styles/vars.scss";
+    .nav-wrap {
+        align-items: center;
+        width: calc(100% - $padding * 2);
+        justify-content: space-between;
+        // padding: 1rem $padding;
+        height: 100px;
+    }
+    .nav-img {
+        img {
+            max-width: 7rem;
+        }
+    }
+    // .nav-menu {
+    // }
+    .nav-menu-desktop {
+        display: none;
+    }
+    .nav-menu-mobile {
+        .hamburger-box{
+            width: 20px;
+        }
+        .hamburger-inner,
+        .hamburger-inner::before,
+        .hamburger-inner::after {
+            background-color: $systemLight;
+            width: 20px;
+        }
+        .is-active {
+            .hamburger-inner,
+            .hamburger-inner::before,
+            .hamburger-inner::after {
+                background-color: $systemLight;
+                // width: 20px;
+            }
+        }
+    }
+</style>
---
+++ src/components/modalMenu.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { createEventDispatcher } from "svelte";
+	import { slide } from "svelte/transition";
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<div class="modal" in:slide out:slide>
+	<a href="#proc-my" class="nav-menu-item">Proč my</a>
+	<a href="#o-systemu" class="nav-menu-item">O systému</a>
+	<a href="#cenik" class="nav-menu-item">Ceník</a>
+	<a href="#kontakt" class="nav-menu-item">Kontakt</a>
+	<div class="space" />
+</div>
+
+<style lang="scss">
+	@import "../styles/vars.scss";
+	.modal {
+		height: calc(100vh - 100px);
+		width: 100vw;
+		// padding: 0rem $padding-basic;
+		background-color: #ffffff;
+		z-index: 998;
+		position: fixed;
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+		justify-content: center;
+		align-items: center;
+		overflow-y: hidden;
+		> a {
+			font-size: 2rem;
+			font-weight: 600;
+			letter-spacing: 0.2rem;
+			text-decoration: none;
+			transition: 0.25s ease;
+			&:hover {
+				transform: scale(1.3);
+				color: $systemDark;
+			}
+		}
+	}
+	.space {
+		height: 100px;
+	}
+	@media only screen and (min-width: 768px) {
+	}
+	@media only screen and (min-width: 1024px) {
+		.modal {
+			width: calc(100% - ($padding-1024 * 2));
+			padding: 0rem $padding-1024;
+		}
+	}
+	@media only screen and (min-width: 1280px) {
+	}
+	@media only screen and (min-width: 1536px) {
+		.modal {
+			width: calc(100% - ($padding-1536 * 2));
+			padding: 0rem $padding-1536;
+		}
+	}
+</style>
